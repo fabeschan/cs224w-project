ca64c0f1845b194bbaae2daaf5cca7dcbd0b6795
Fabian Chan <fabeschan@gmail.com>
2016-11-08 22:21:40 -0800

    add Get*Centr.txt for 0-2 and centrality.py

--- centrality.py
+++ centrality.py
@@ -1,36 +1,44 @@
 import snap
+import parser, make_graphs
 
-graph = snap.LoadEdgeList(snap.PUNGraph, "imdb_actor_edges.tsv", 0, 1, '\t')
+filenames = [ "0301/{}.txt".format(i) for i in range(0, 2) ]
+data = parser.Data(filenames)
+graph = make_graphs.make_graph(data)
+ugraph = snap.ConvertGraph(snap.PUNGraph, graph)
 mxwcc = snap.GetMxWcc(graph)
+umxwcc = snap.GetMxWcc(ugraph)
 N = 20
 
-# === 2.1 ===
+# === GetDegreeCentr ===
 s = []
-for NI in mxwcc.Nodes():
-    DegCentr = snap.GetDegreeCentr(mxwcc, NI.GetId())
+for NI in umxwcc.Nodes():
+    DegCentr = snap.GetDegreeCentr(umxwcc, NI.GetId())
     s.append((NI.GetId(), DegCentr))
 s.sort(key=lambda x: x[1], reverse=True) # sort with max centrality at front
-print '=== 2.1 ==='
-for x in s[:N]:
-    print x
+print '=== GetDegreeCentr ==='
+with open("GetDegreeCentr-0-2.txt", 'w') as f:
+    for x in s:
+        f.write("{} {}\n".format(*x))
 
-# === 2.2 ===
+# === GetBetweennessCentr ===
 Nodes = snap.TIntFltH()
 Edges = snap.TIntPrFltH()
 snap.GetBetweennessCentr(mxwcc, Nodes, Edges, 1.0)
 s = [ (node, Nodes[node]) for node in Nodes ]
 s.sort(key=lambda x: x[1], reverse=True) # sort with max centrality at front
-print '=== 2.2 ==='
-for x in s[:N]:
-    print x
+print '=== GetBetweennessCentr ==='
+with open("GetBetweennessCentr-0-2.txt", 'w') as f:
+    for x in s:
+        f.write("{} {}\n".format(*x))
 
-# === 2.3 ===
+# === GetClosenessCentr ===
 s = []
 for NI in mxwcc.Nodes():
     CloseCentr = snap.GetClosenessCentr(mxwcc, NI.GetId())
     s.append((NI.GetId(), CloseCentr))
 s.sort(key=lambda x: x[1], reverse=True) # sort with max centrality at front
-print '=== 2.3 ==='
-for x in s[:N]:
-    print x
+print '=== GetClosenessCentr ==='
+with open("GetClosenessCentr-0-2.txt", 'w') as f:
+    for x in s:
+        f.write("{} {}\n".format(*x))
 
