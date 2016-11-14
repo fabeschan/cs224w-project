c73bf1014c721df45f6177901df18ce69e2fc429
Fabian Chan <fabeschan@gmail.com>
2016-11-14 12:26:08 -0800

    add plot_centrality_age

---
+++ plot_centrality_age.py
@@ -0,0 +1,24 @@
+import parser, make_graphs
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerLine2D
+
+
+if __name__ == '__main__':
+    filenames = [ "0301/{}.txt".format(i) for i in range(0, 2) ]
+    data = parser.Data(filenames)
+    graph = make_graphs.make_graph(data)
+
+    centr_files = ['GetBetweennessCentr-0-2.txt', 'GetClosenessCentr-0-2.txt', 'GetDegreeCentr-0-2.txt']
+    for centr in centr_files:
+        with open(centr) as f:
+            contents = f.readlines()
+        split = [ l.strip().split(' ') for l in contents ]
+        parsed = [ (int(nid), float(centr)) for nid, centr in split ]
+        results = [ (data.lookup[data.videoid[nid]].age, centr) for nid, centr in parsed ]
+        y, x = zip(*results)
+        print y[:20]
+        print x[:20]
+
+        plt.scatter(x, y)
+        plt.show()
