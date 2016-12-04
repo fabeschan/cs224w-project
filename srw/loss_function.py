050012501964091279755207451b28e5d9bedf0c
Fabian Chan <fabeschan@gmail.com>
2016-12-04 02:08:41 -0800

    Add more srw stuff and walk.py

--- srw/loss_function.py
+++ srw/loss_function.py
@@ -6,11 +6,3 @@ def squared_hinge_loss(x):
 
 def squared_hinge_loss_derivative(x):
     return 2 * x if x > 0 else 0
-
-'''
-def evaluate(self, p, w):
-    # evaluate the total loss where p is the pagerank vector and w the weight vector
-    shl = np.vectorize(self.squared_hinge_loss)
-    t = shl(np.dot(p.T, w))
-    return np.sum(t)
-'''
--- srw/transition_matrix.py
+++ srw/transition_matrix.py
@@ -1,55 +1,59 @@
 import numpy as np
 import loss_function
-
-def f_ext(u, v, kb):
-    x = [1, 0, 0.5]
-    return np.array(x)
+import features
 
 def activation_function(x_uv, w):
-    return np.exp(np.dot(w, x_uv))
+    return np.exp(-np.dot(x_uv, w))
 
-def feature_matrix(g, f_ext)
-    x = np.zeros((g.GetNodes(), g.GetNodes()))
+def feature_matrix(g, fx):
+    x = np.zeros((g.GetNodes(), g.GetNodes(), fx.NUM_FEATURES))
 
     for u_NI in g.Nodes():
         u = u_NI.GetId()
         for v_NI in g.Nodes():
             v = v_NI.GetId()
-            x_uv = f_ext.calculate(u, v)
-            x[u, v] = x_uv
+            x_uv = fx.feature_vector(u, v)
+            x[u][v] = x_uv
     return x
 
 def edge_weights(w, fm, activation_function):
     ''' return matrix of edge weights no normalization '''
     # fm: the calculated feature_matrix
-    v = np.vectorize(activation_function, excluded=['w'])
-    A = v(fm, w=w)
+    A = np.zeros([fm.shape[0], fm.shape[1]])
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            A[i][j] = activation_function(fm[i][j], w)
     return A
 
 def normalize(A):
     n = np.sum(A)
     return A / n
 
-def transition_matrix(w, fm):
-    return normalize(edge_weights(w, fm, activation_function))
+def transition_matrix(w, fm, am):
+    '''
+    w: weights
+    fm: feature matrix
+    am: adjacency matrix
+    '''
 
-def pagerank_one_iter(p, w, fm, alpha=0.85):
-    tm = transition_matrix(w, fm)
-    return (1 - alpha) * np.matul(p, tm) + alpha * p
+    return normalize(edge_weights(w, fm, activation_function) * am)
 
-def p_grad_w(p, w, fm, delta=10**-12):
-    fp = pagerank_one_iter
-    grad = zeros((len(p), len(w))
-    for pi in range(len(p)):
-        for wi in range(len(wi)):
-            ww = np.copy(w)
-            ww[wi] += delta
-            grad[pi][wi] = (fp(p, ww, fm) - fp(p, w, fm)) / delta
-   return grad
+def pagerank_one_iter(p, w, fm, am, alpha=0.15):
+    tm = transition_matrix(w, fm, am)
+    r = (1 - alpha) * np.matmul(p, tm) + alpha * p
+    return r
 
-def j_grad_w(p, w, fm):
-    pgw = p_grad_w(p, w, fm)
-    grad = zeros([len(w)])
+def p_grad_w(p, w, fm, am, delta=10**-15):
+    grad = np.zeros((len(p), len(w)))
+    for wi in range(len(w)):
+        ww = np.copy(w)
+        ww[wi] += delta
+        grad[:,wi] = (pagerank_one_iter(p, ww, fm, am) - pagerank_one_iter(p, w, fm, am)) / delta
+    return grad
+
+def j_grad_w(p, w, fm, am):
+    pgw = p_grad_w(p, w, fm, am)
+    grad = np.zeros([len(w)])
     for wi in range(len(w)):
         s = 0
         for i in range(len(p)):
@@ -58,8 +62,8 @@ def j_grad_w(p, w, fm):
         grad[wi] = s
     return grad
 
-def gradient_descent_step(p, w, fm, r=0.01):
-    new_w = w - r * j_grad_w(p, w, fm)
+def gradient_descent_step(p, w, fm, am, r=0.01):
+    new_w = w - r * j_grad_w(p, w, fm, am)
     return new_w
 
 
